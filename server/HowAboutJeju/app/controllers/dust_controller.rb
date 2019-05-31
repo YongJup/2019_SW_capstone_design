1ae76670a57d71599d738ab1ccf372a4aad0738a
YongJup <k33721@gmail.com>
2019-06-01 07:44:17 +0900

    add controller and model

---
+++ server/HowAboutJeju/app/controllers/dust_controller.rb
@@ -0,0 +1,7 @@
+class DustController < ApplicationController
+  def update_data
+  end
+
+  def get_data
+  end
+end
--- server/HowAboutJeju/config/routes.rb
+++ server/HowAboutJeju/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'dust/update_data'
+  get 'dust/get_data'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
