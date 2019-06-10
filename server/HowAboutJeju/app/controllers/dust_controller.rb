19ff0ce5f3ea92b5193f83c5c210e0d18f8745e4
Yongjae <k33721@gmail.com>
2019-06-10 21:58:33 +0900

    update update_data

--- server/HowAboutJeju/app/controllers/dust_controller.rb
+++ server/HowAboutJeju/app/controllers/dust_controller.rb
@@ -1,25 +1,34 @@
 class DustController < ApplicationController
-  
-  def set_data
-
-  end
-  
   def update_data
     key = ''
     url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey="+ key + "&sidoName=#{URI.encode('제주')}&ver=1.3&_returnType=json"
     page = RestClient.get(url)
     doc = JSON.parse(page)
     
-    (0..4).each do |i|
-      air = Air.new
-      air.name = doc['list'][i]['stationName']
-      air.dataTime = doc['list'][i]['dataTime']
-      air.pm10Value = doc['list'][i]['pm10Value']
-      air.pm10Grade = doc['list'][i]['pm10Grade']
-      air.pm25Value = doc['list'][i]['pm25Value']
-      air.pm25Grade = doc['list'][i]['pm25Grade']
-      air.airGrade = doc['list'][i]['khaiGrade']
-      air.save
+    if (Air.all == []) then
+      (0..4).each do |i|
+        air = Air.new
+        air.name = doc['list'][i]['stationName']
+        air.dataTime = doc['list'][i]['dataTime']
+        air.pm10Value = doc['list'][i]['pm10Value']
+        air.pm10Grade = doc['list'][i]['pm10Grade']
+        air.pm25Value = doc['list'][i]['pm25Value']
+        air.pm25Grade = doc['list'][i]['pm25Grade']
+        air.airGrade = doc['list'][i]['khaiGrade']
+        air.save
+      end
+    else
+      (1..5).each do |i|
+        air = Air.find i
+        air.name = doc['list'][i]['stationName']
+        air.dataTime = doc['list'][i]['dataTime']
+        air.pm10Value = doc['list'][i]['pm10Value']
+        air.pm10Grade = doc['list'][i]['pm10Grade']
+        air.pm25Value = doc['list'][i]['pm25Value']
+        air.pm25Grade = doc['list'][i]['pm25Grade']
+        air.airGrade = doc['list'][i]['khaiGrade']
+        air.save
+      end
     end
   end
 
